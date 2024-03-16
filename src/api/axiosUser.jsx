fa588978b2b2c6b88f610be4f637b5b21393d8ac
Kavindu <kavindu2308@gmail.com>
2024-03-17 01:02:55 +0530

    #004 Complete the backend integration

--- src/api/axiosUser.jsx
+++ src/api/axiosUser.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const user = axios.create({
     withCredentials: true,
-    baseURL: "http://localhost:4000/"
+    baseURL: "http://localhost:4000/api/"
 });
 
 export default user;
